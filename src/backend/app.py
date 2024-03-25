0425e3548f9fdd4855c179db6019f27e7295d381
Matei Teodoru <mateiteodoru@RN-145-90-177-81.eduroam.rug.nl>
2024-03-25 18:12:01 +0100

    test

--- src/backend/app.py
+++ src/backend/app.py
@@ -13,24 +13,26 @@ def home():
 @inject
 @app.route("/upload_image", methods=["POST"])
 def upload(wildbook: Wildbook = world[Wildbook]):
-    if "image" not in request.files:
+    if "image" not in request.files or "image_name" not in request.form:  # Check for image and image_name
         return redirect(request.url)
+    
     image = request.files["image"]
+    image_name = request.form["image_name"]  # Capture the name from the text input
+    
     if image.filename == "":
         return redirect(request.url)
 
     # Save the file temporarily
-    temp_image_path = (
-        "path_to_temp_storage"  # Specify the path to save the temporary file
-    )
+    temp_image_path = "path_to_temp_storage"  # Specify the path to save the temporary file
     image.save(temp_image_path)
 
     # Call the upload_image method on the instance of Wildbook
     image_id = wildbook.upload_image(temp_image_path)
 
+    wildbook.rename_annotations([image_id], [image_name])  
+    
     # Handle the response
-    return str(image_id)
-
+    return str("Image with id " + image_id + " uploaded succesfully with name " + image_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
