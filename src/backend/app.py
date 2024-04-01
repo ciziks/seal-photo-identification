cf0e45529d516cc5adfb64b3e2874bef5239c994
LucasCiziks <lucas.ciziks@loft.com.br>
2024-04-01 13:20:37 +0200

    Automatically labeling match seals

--- src/backend/app.py
+++ src/backend/app.py
@@ -35,13 +35,11 @@ def seal_form():
 @inject
 @app.route("/seal", methods=["POST"])
 def new_seal(wildbook: Wildbook = world[Wildbook]):
-    if (
-        "image" not in request.files or "image_name" not in request.form
-    ):  # Check for image and image_name
+    if "image" not in request.files:  # Check for image
         return redirect(request.url)
 
     image = request.files["image"]
-    image_name = request.form["image_name"]  # Capture the name from the text input
+    # image_name = request.form["image_name"]  # Capture the name from the text input
 
     if image.filename == "":
         return redirect(request.url)
@@ -56,11 +54,6 @@ def new_seal(wildbook: Wildbook = world[Wildbook]):
     # Detect the seal in the image
     aid_list = wildbook.detect_seal([int(image_id)])
 
-    # Set the name of the seal
-    wildbook.rename_annotations(
-        aid_list, [image_name]
-    )  # Rename the uploaded image with the provided name
-
     # Match seal with seals in DB
     score = wildbook.seal_matching(aid_list[0])
 
@@ -68,7 +61,6 @@ def new_seal(wildbook: Wildbook = world[Wildbook]):
     try:
         match_aid, match_score = next(iter(score.items()))
     except StopIteration:
-
         # Handle the case where there are no items
         match_aid, match_score = None, None
 
@@ -78,6 +70,13 @@ def new_seal(wildbook: Wildbook = world[Wildbook]):
     # If there is a match, get the 'best match' image
     if match_aid:
         matched_image = wildbook.get_annotation_image(match_aid)
+        matched_name = wildbook.get_annotation_name(match_aid)
+
+        # Set the name of the seal
+        wildbook.rename_annotations(
+            aid_list, [matched_name]
+        )  # Rename the uploaded image with the match name
+
         matched_image_base64 = matched_image.split(",", 1)[1]  # Remove the prefix
     else:
         # If there is no match, set a placeholder
@@ -91,16 +90,18 @@ def new_seal(wildbook: Wildbook = world[Wildbook]):
         initial_image=initial_image_base64,
         matched_image=matched_image_base64,
         match_aid=match_aid,
+        match_name=matched_name,
         match_score=match_score,
-        initial_aid=aid_list[0]
+        initial_aid=aid_list[0],
     )
+
 
 # Handle the request to list seals stored inside the database
 @inject
 @app.route("/list_seals")
 def list_seals(wildbook: Wildbook = world[Wildbook]):
 
-    #Get the list of aids
+    # Get the list of aids
     seal_aids = wildbook.list_annotations_id()
     seals_data = []
 
@@ -110,10 +111,10 @@ def list_seals(wildbook: Wildbook = world[Wildbook]):
             seal_name = wildbook.get_annotation_name(aid)
             image_url = wildbook.get_annotation_image(aid)
 
-            seal = {'name': seal_name, 'image_url': image_url}
+            seal = {"name": seal_name, "image_url": image_url}
             seals_data.append(seal)
         except Exception as e:
-            print(f"An error occurred: {e}") 
+            print(f"An error occurred: {e}")
 
     # Return template with the data
     return render_template("list_seals.html", seals=seals_data)
